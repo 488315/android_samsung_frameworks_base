990ea2962bdd0664d3dc023521b82eb7eb6043fc
Kai Jones <kvjones0243@gmail.com>
2025-01-29 17:09:13 -0800

    Initial commit for print
    Pulled from Samsung services.jar

---
+++ services/print/java/com/android/server/print/C2439x41132d4b.java
@@ -0,0 +1,15 @@
+package com.android.server.print;
+
+import com.android.server.print.UserState;
+import java.util.List;
+import java.util.function.BiConsumer;
+
+/* compiled from: R8$$SyntheticClass */
+/* renamed from: com.android.server.print.UserState$PrinterDiscoverySessionMediator$$ExternalSyntheticLambda7 */
+/* loaded from: classes3.dex */
+public final /* synthetic */ class C2439x41132d4b implements BiConsumer {
+    @Override // java.util.function.BiConsumer
+    public final void accept(Object obj, Object obj2) {
+        ((UserState.PrinterDiscoverySessionMediator) obj).handleDispatchPrintersRemoved((List) obj2);
+    }
+}
