9e9498e7ebdaf0da0a88744ed2c5ddd85c9f7d8a
Kai Jones <kvjones0243@gmail.com>
2025-01-29 17:09:12 -0800

    Initial commit for credentials
    Pulled from Samsung services.jar

---
+++ services/credentials/java/com/android/server/credentials/C1087x1051f624.java
@@ -0,0 +1,14 @@
+package com.android.server.credentials;
+
+import android.credentials.CredentialOption;
+import java.util.function.Function;
+
+/* compiled from: R8$$SyntheticClass */
+/* renamed from: com.android.server.credentials.CredentialManagerService$CredentialManagerServiceStub$$ExternalSyntheticLambda3 */
+/* loaded from: classes.dex */
+public final /* synthetic */ class C1087x1051f624 implements Function {
+    @Override // java.util.function.Function
+    public final Object apply(Object obj) {
+        return ((CredentialOption) obj).getType();
+    }
+}
